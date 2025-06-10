3a855293ed6046d8485713d273df9a7f93456bdb
Cenk Korkmaz <Cenk.Korkmaz92@gmail.com>
2025-06-10 14:46:25 +0200

    added authentication views for registration and login

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import RegistrationSerializer
+from .serializers import RegistrationSerializer, LoginSerializer
 from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token
+from django.contrib.auth import authenticate
 
 class RegistrationView(APIView):
     def post(self, request):
@@ -18,4 +19,27 @@ class RegistrationView(APIView):
                 "email": user.email,
                 "user_id": user.id
             }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class LoginView(APIView):
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid():
+            email = serializer.validated_data['email']
+            password = serializer.validated_data['password']
+            try:
+                user = User.objects.get(email=email)
+            except User.DoesNotExist:
+                return Response({'error': 'Invalid credentials.'}, status=status.HTTP_400_BAD_REQUEST)
+            user = authenticate(username=user.username, password=password)
+            if user is not None:
+                token, created = Token.objects.get_or_create(user=user)
+                return Response({
+                    "token": token.key,
+                    "fullname": user.first_name,
+                    "email": user.email,
+                    "user_id": user.id
+                }, status=status.HTTP_200_OK)
+            else:
+                return Response({'error': 'Invalid credentials.'}, status=status.HTTP_400_BAD_REQUEST)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
