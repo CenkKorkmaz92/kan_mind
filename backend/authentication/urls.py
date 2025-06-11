dd1f4cc4e0e4111a87ab48810cea5d310e6a27bf
Cenk Korkmaz <Cenk.Korkmaz92@gmail.com>
2025-06-11 09:27:32 +0200

    added boards endpoint to authentication URLs

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import RegistrationView, LoginView
+from .views import RegistrationView, LoginView, BoardListCreateView
 
 urlpatterns = [
     path('registration/', RegistrationView.as_view(), name='registration'),
     path('login/', LoginView.as_view(), name='login'),
+    path('boards/', BoardListCreateView.as_view(), name='boards'),
 ]
